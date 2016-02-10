f8179e4e506e19b3d0df1245ee6d7f21414cec33
Issaq Al-Ahmed <ialahmed@pivotal.io>
2016-02-10 15:30:10 -0800

    Fetches OpenID Token from UAA
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

---
+++ authcode/src/main/java/org/cloudfoundry/identity/oauth2/composite/CompositeAccessTokenProvider.java
@@ -0,0 +1,149 @@
+package org.cloudfoundry.identity.oauth2.composite;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.client.filter.state.DefaultStateKeyGenerator;
+import org.springframework.security.oauth2.client.filter.state.StateKeyGenerator;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.resource.UserApprovalRequiredException;
+import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
+import org.springframework.security.oauth2.client.token.*;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidRequestException;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+
+public class CompositeAccessTokenProvider extends AuthorizationCodeAccessTokenProvider {
+
+    private StateKeyGenerator stateKeyGenerator = new DefaultStateKeyGenerator();
+    private boolean stateMandatory = true;
+
+    public CompositeAccessTokenProvider() {
+        super.setStateKeyGenerator(stateKeyGenerator);
+    }
+
+    @Override
+    public OAuth2AccessToken obtainAccessToken(OAuth2ProtectedResourceDetails details, AccessTokenRequest request)
+        throws UserRedirectRequiredException, UserApprovalRequiredException, AccessDeniedException,
+        OAuth2AccessDeniedException {
+
+        AuthorizationCodeResourceDetails resource = (AuthorizationCodeResourceDetails) details;
+
+        if (request.getAuthorizationCode() == null) {
+            if (request.getStateKey() == null) {
+                throw getRedirectForAuthorization(resource, request);
+            }
+            obtainAuthorizationCode(resource, request);
+        }
+        return retrieveToken(request, resource, getParametersForTokenRequest(resource, request),
+            getHeadersForTokenRequest(request));
+
+    }
+
+    private HttpHeaders getHeadersForTokenRequest(AccessTokenRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        // No cookie for token request
+        return headers;
+    }
+
+    private MultiValueMap<String, String> getParametersForTokenRequest(AuthorizationCodeResourceDetails resource,
+                                                                       AccessTokenRequest request) {
+
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+        form.set("grant_type", "authorization_code");
+        form.set("code", request.getAuthorizationCode());
+
+        Object preservedState = request.getPreservedState();
+        if (request.getStateKey() != null || stateMandatory) {
+            // The token endpoint has no use for the state so we don't send it back, but we are using it
+            // for CSRF detection client side...
+            if (preservedState == null) {
+                throw new InvalidRequestException(
+                    "Possible CSRF detected - state parameter was required but no state could be found");
+            }
+        }
+
+        // Extracting the redirect URI from a saved request should ignore the current URI, so it's not simply a call to
+        // resource.getRedirectUri()
+        String redirectUri = null;
+        // Get the redirect uri from the stored state
+        if (preservedState instanceof String) {
+            // Use the preserved state in preference if it is there
+            // TODO: treat redirect URI as a special kind of state (this is a historical mini hack)
+            redirectUri = String.valueOf(preservedState);
+        }
+        else {
+            redirectUri = resource.getRedirectUri(request);
+        }
+
+        if (redirectUri != null && !"NONE".equals(redirectUri)) {
+            form.set("redirect_uri", redirectUri);
+        }
+
+        return form;
+
+    }
+
+    private UserRedirectRequiredException getRedirectForAuthorization(AuthorizationCodeResourceDetails resource,
+                                                                      AccessTokenRequest request) {
+
+        // we don't have an authorization code yet. So first get that.
+        TreeMap<String, String> requestParameters = new TreeMap<String, String>();
+        requestParameters.put("response_type", "code+id_token");
+        requestParameters.put("client_id", resource.getClientId());
+        // Client secret is not required in the initial authorization request
+
+        String redirectUri = getIdTokenRedirectUri(request);
+        if (redirectUri != null) {
+            requestParameters.put("redirect_uri", redirectUri);
+        }
+
+        if (resource.isScoped()) {
+
+            StringBuilder builder = new StringBuilder();
+            List<String> scope = resource.getScope();
+
+            if (scope != null) {
+                Iterator<String> scopeIt = scope.iterator();
+                while (scopeIt.hasNext()) {
+                    builder.append(scopeIt.next());
+                    if (scopeIt.hasNext()) {
+                        builder.append(' ');
+                    }
+                }
+            }
+
+            requestParameters.put("scope", builder.toString());
+        }
+
+        UserRedirectRequiredException redirectException = new UserRedirectRequiredException(
+            resource.getUserAuthorizationUri(), requestParameters);
+
+        String stateKey = stateKeyGenerator.generateKey(resource);
+        redirectException.setStateKey(stateKey);
+        request.setStateKey(stateKey);
+        redirectException.setStateToPreserve(redirectUri);
+        request.setPreservedState(redirectUri);
+
+        return redirectException;
+
+    }
+
+    public String getIdTokenRedirectUri(AccessTokenRequest request) {
+        try {
+            URL url = new URL(request.getCurrentUri());
+            return url.getProtocol() + "://" + url.getAuthority() + "/id_token.html";
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
+
--- authcode/src/main/java/org/cloudfoundry/identity/samples/authcode/Application.java
+++ authcode/src/main/java/org/cloudfoundry/identity/samples/authcode/Application.java
@@ -1,23 +1,44 @@
 package org.cloudfoundry.identity.samples.authcode;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.apache.commons.codec.binary.Base64;
+import org.cloudfoundry.identity.oauth2.composite.CompositeAccessTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.AccessTokenProvider;
+import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 
 @Configuration
 @EnableAutoConfiguration
@@ -43,13 +64,26 @@ public class Application {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @PostConstruct
+    public void init() {
+        oauth2RestTemplate.setAccessTokenProvider(accessTokenProviderChain());
+    }
+
+    @Bean
+    public AccessTokenProvider accessTokenProviderChain() {
+        return new AccessTokenProviderChain(Arrays.<AccessTokenProvider> asList(
+            new CompositeAccessTokenProvider(),
+            new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
+            new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider()));
+    }
+
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
     @RequestMapping("/authorization_code")
-    public String authCode(Model model) throws Exception {
+    public String authCode(Model model, HttpSession session) throws Exception {
         if (oauth2RestTemplate == null) {
             return "configure_warning";
         }
@@ -59,14 +93,19 @@ public class Application {
         model.addAttribute("response",toPrettyJsonString(userInfoResponse));
         Map<String, ?> token = getToken(oauth2RestTemplate.getOAuth2ClientContext());
         model.addAttribute("token",toPrettyJsonString(token));
+        model.addAttribute("id_token", toPrettyJsonString(parseToken((String) session.getAttribute("openid_token"))));
         return "authorization_code";
+    }
+
+    private Map<String, ?> parseToken(String base64Token) throws IOException {
+        String token = base64Token.split("\\.")[1];
+        return objectMapper.readValue(Base64.decodeBase64(token), new TypeReference<Map<String, ?>>() {
+        });
     }
 
     private Map<String, ?> getToken(OAuth2ClientContext clientContext) throws Exception {
         if (clientContext.getAccessToken() != null) {
-            String tokenBase64 = clientContext.getAccessToken().getValue().split("\\.")[1];
-            return objectMapper.readValue(Base64.decodeBase64(tokenBase64), new TypeReference<Map<String, ?>>() {
-            });
+            return parseToken(clientContext.getAccessToken().getValue());
         }
         return null;
     }
@@ -74,4 +113,11 @@ public class Application {
     private String toPrettyJsonString(Object object) throws Exception {
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
     }
+
+    @RequestMapping(value = "/save_id_token", method = GET)
+    public ResponseEntity<Void> saveIdToken(@RequestParam(value = "id_token") String idToken, HttpSession session) {
+        session.setAttribute("openid_token", idToken);
+        return new ResponseEntity<Void>(CREATED);
+    }
+
 }
