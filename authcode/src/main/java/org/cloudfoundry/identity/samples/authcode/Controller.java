b830a1cdbb27019e6e646ace5aba41e8cdcdf8e9
Helen Chung <hchung@pivotal.io>
2016-03-14 10:08:14 -0700

    Removes app logout link from authcode sample.
    [Finishes #115528503] https://www.pivotaltracker.com/story/show/115528503
    
    Signed-off-by: Kenny Wang <kwang@pivotal.io>

--- authcode/src/main/java/org/cloudfoundry/identity/samples/authcode/Controller.java
+++ authcode/src/main/java/org/cloudfoundry/identity/samples/authcode/Controller.java
@@ -47,13 +47,4 @@ public class Controller {
         todoService.delete(id);
         return "redirect:/todo";
     }
-
-    @RequestMapping(value="/logout", method = GET)
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/";
-    }
 }
