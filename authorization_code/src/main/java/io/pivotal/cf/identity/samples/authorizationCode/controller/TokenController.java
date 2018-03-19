2b66ac2b0abf8a0d27e3955c67a9ab47a6c3a1e8
Abhi Hiremagalur <ahiremagalur@pivotal.io>
2018-03-19 12:09:36 -0700

    Remove superflous code
    Also avoid masking exceptions to make app easier to debug
    
    Signed-off-by: Oliver Albertini <oalbertini@pivotal.io>

--- authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
+++ authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
@@ -1,5 +1,6 @@
 package io.pivotal.cf.identity.samples.authorizationCode.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
@@ -20,11 +21,10 @@ public class TokenController {
     private ObjectMapper objectMapper;
 
     @RequestMapping(value = "/secured/token")
-    public String show() {
+    public String show() throws IOException {
         OAuth2Authentication auth = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
         OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)auth.getDetails();
-        String tokenValue = details.getTokenValue();
-        
+
         return "<html>\n" +
                 "<body>\n" +
                 "\n" +
@@ -32,27 +32,18 @@ public class TokenController {
                 "    <button id=\"logout\" type=\"submit\">Logout</button>\n" +
                 "</form>\n" +
                 "\n" +
-                "<pre>" + formatJwtToken(tokenValue) + "</pre>\n" +
+                "<pre>" + prettyPrint(decode(details.getTokenValue())) + "</pre>\n" +
                 "\n" +
                 "</body>\n" +
                 "</html>";
     }
 
-    private String formatJwtToken(String token) {
-        try {
-            return toPrettyJsonString(parseToken(token));
-        } catch (Exception e) {
-            return "";
-        }
-    }
-
-    private Map<String, ?> parseToken(String base64Token) throws IOException {
+    private Map<String, ?> decode(String base64Token) throws IOException {
         String token = base64Token.split("\\.")[1];
-        return this.objectMapper.readValue(Base64.decodeBase64(token),
-                new TypeReference<Map<String, ?>>() {});
+        return this.objectMapper.readValue(Base64.decodeBase64(token), new TypeReference<Map<String, ?>>() {});
     }
 
-    private String toPrettyJsonString(Object object) throws Exception {
+    private String prettyPrint(Object object) throws JsonProcessingException {
         return this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
     }
 }
