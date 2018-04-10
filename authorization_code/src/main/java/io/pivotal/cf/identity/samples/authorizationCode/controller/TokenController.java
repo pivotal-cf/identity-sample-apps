83d7646acf7b566329573a3e9ad5383191c6e047
Alex Jackson <ajackson@pivotal.io>
2018-04-10 15:40:34 -0700

    Refactor to let spring inject oauth principal
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
+++ authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +20,8 @@ public class TokenController {
     private ObjectMapper objectMapper;
 
     @RequestMapping(value = "/secured/token")
-    public String show() throws IOException {
-        OAuth2Authentication auth = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)auth.getDetails();
+    public String show(OAuth2Authentication principal) throws IOException {
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) principal.getDetails();
 
         return "<html>\n" +
                 "<body>\n" +
