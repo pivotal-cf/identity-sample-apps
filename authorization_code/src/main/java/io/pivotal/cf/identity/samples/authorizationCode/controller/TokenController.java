74286ce920adc68052b4a35964404d90bee60c56
Aakash Shah <ashah@pivotal.io>
2018-04-10 16:10:45 -0700

    Use JwtHelper to decode token
    Signed-off-by: Alex Jackson <ajackson@pivotal.io>

--- authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
+++ authorization_code/src/main/java/io/pivotal/cf/identity/samples/authorizationCode/controller/TokenController.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,7 @@ public class TokenController {
     public String show(OAuth2Authentication principal) throws IOException {
         OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) principal.getDetails();
 
+        Jwt decodedToken = JwtHelper.decode(details.getTokenValue());
         return "<html>\n" +
                 "<body>\n" +
                 "\n" +
@@ -30,18 +33,15 @@ public class TokenController {
                 "    <button id=\"logout\" type=\"submit\">Logout</button>\n" +
                 "</form>\n" +
                 "\n" +
-                "<pre>" + prettyPrint(decode(details.getTokenValue())) + "</pre>\n" +
+                "<pre>" + prettyPrint(decodedToken.getClaims()) + "</pre>\n" +
                 "\n" +
                 "</body>\n" +
                 "</html>";
     }
 
-    private Map<String, ?> decode(String base64Token) throws IOException {
-        String token = base64Token.split("\\.")[1];
-        return this.objectMapper.readValue(Base64.decodeBase64(token), new TypeReference<Map<String, ?>>() {});
-    }
-
-    private String prettyPrint(Object object) throws JsonProcessingException {
-        return this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+    private String prettyPrint(String decodedTokenJson) throws IOException {
+        return this.objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(this.objectMapper.readValue(decodedTokenJson, Object.class));
     }
 }
