c688e38c9fa1411a98d514f88071c97208d517ec
Peter Chen <pchen@pivotal.io>
2019-04-15 16:40:02 -0700

    Simplify issuer check [#163529920]
    Signed-off-by: Steven Miyakawa <smiyakawa@pivotal.io>

--- resource-server/src/main/java/io/pivotal/identityService/samples/resourceserver/configuration/ServerConfiguration.java
+++ resource-server/src/main/java/io/pivotal/identityService/samples/resourceserver/configuration/ServerConfiguration.java
@@ -5,30 +5,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoderJwkSupport;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 public class ServerConfiguration {
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwksUri;
-
-    @Value("${sso.issuer}")
-    private String issuer;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     @Value("${sso.audience:}")
     private String audience;
 
     @Bean
     JwtDecoder jwtDecoder() {
-        NimbusJwtDecoderJwkSupport jwtDecoder = new NimbusJwtDecoderJwkSupport(jwksUri);
+        NimbusJwtDecoderJwkSupport jwtDecoder = (NimbusJwtDecoderJwkSupport)
+                JwtDecoders.fromOidcIssuerLocation(issuerUri); // spring docs says use withOidcIssuerLocation
 
         // Validate `iss` and `aud` claims from application.yml
-        jwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(
-                JwtValidators.createDefaultWithIssuer(issuer),
-                new AudienceValidator(audience)
-        ));
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
 
         return jwtDecoder;
     }
