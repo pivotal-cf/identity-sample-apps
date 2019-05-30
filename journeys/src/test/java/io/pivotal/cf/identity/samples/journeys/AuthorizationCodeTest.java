4e79482a33c1d9ce42804680c1d4f261ae91bcc7
Tyler Schultz <tchultz@pivotal.io>
2019-05-30 16:53:28 -0700

    Journey test shows basic user denied access
    [#164965282]
    
    Signed-off-by: Peter Chen <pchen@pivotal.io>

--- journeys/src/test/java/io/pivotal/cf/identity/samples/journeys/AuthorizationCodeTest.java
+++ journeys/src/test/java/io/pivotal/cf/identity/samples/journeys/AuthorizationCodeTest.java
@@ -5,6 +5,7 @@ import org.fluentlenium.core.hook.wait.Wait;
 import org.junit.Test;
 
 import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.containingText;
 
 
 @Wait
@@ -12,7 +13,7 @@ public class AuthorizationCodeTest extends FluentTest {
     public static final String AUTHCODE_CLIENT_BASE_URL = "http://localhost:8888";
 
     @Test
-    public void displaysAccessTokenToAllUsers() {
+    public void displaysTokensToAllUsers() {
         goTo(AUTHCODE_CLIENT_BASE_URL + "/info");
 
         $("input[name=username]").fill().with("basic-user");
@@ -23,11 +24,14 @@ public class AuthorizationCodeTest extends FluentTest {
             $("#authorize").click();
         }
 
-        //TODO: consider parsing the stringified JSON and asserting on specific values
-        assertThat(el("body").text()).contains("user@example.com"); //TODO: the actual email is basic-user@example.com, but the test fails. why?
-        assertThat(el("body").text()).contains("basic-user");
-        assertThat(el("body").text()).contains("sample-client");
-        assertThat(el("body").text()).contains("openid");
+        String accessToken = el(".access_token").text();
+        assertThat(accessToken).contains("basic-user@example.com");
+        assertThat(accessToken).contains("basic-user");
+        assertThat(accessToken).contains("sample-client");
+        assertThat(accessToken).contains("openid");
+
+        String userInfo = el(".user_info").text();
+        assertThat(userInfo).contains("FirstName LastName");
 
         $("#logout").click();
         assertThat(url()).isEqualTo(AUTHCODE_CLIENT_BASE_URL + "/");
@@ -36,41 +40,26 @@ public class AuthorizationCodeTest extends FluentTest {
         assertThat(url()).contains("/uaa/login");
     }
 
-//    @Test
-//    public void displaysUserinfoToAllUsers() {
-//        goTo(AUTHCODE_CLIENT_BASE_URL + "/secured/userinfo");
-//
-//        $("input[name=username]").fill().with("basic-user");
-//        $("input[name=password]").fill().with("example-password");
-//        $("input[type=submit]").submit();
-//
-//        if ($("h1").present() && el("h1").text().equals("Application Authorization")) {
-//            $("#authorize").click();
-//        }
-//
-//        assertThat(el("body").text()).contains("FirstName LastName");
-//
-//        $("#logout").click();
-//    }
-//
-//    @Test
-//    public void basicUser() {
-//        goTo(AUTHCODE_CLIENT_BASE_URL + "/secured/abc");
-//
-//        $("input[name=username]").fill().with("basic-user");
-//        $("input[name=password]").fill().with("example-password");
-//        $("input[type=submit]").submit();
-//
-//        if ($("h1").present() && el("h1").text().equals("Application Authorization")) {
-//            $("#authorize").click();
-//        }
-//
-//        assertThat(el("body").text()).contains("Invalid token");
-//
-//        goTo(AUTHCODE_CLIENT_BASE_URL + "/secured/access_token");
-//        $("#logout").click();
-//    }
-//
+    @Test
+    public void basicUserIsDeniedAccessToResource() {
+        goTo(AUTHCODE_CLIENT_BASE_URL + "/info");
+
+        $("input[name=username]").fill().with("basic-user");
+        $("input[name=password]").fill().with("example-password");
+        $("input[type=submit]").submit();
+
+        if ($("h1").present() && el("h1").text().equals("Application Authorization")) {
+            $("#authorize").click();
+        }
+
+        $("a", containingText("TODO List")).click();
+
+        assertThat(el("body").text()).contains("401 UNAUTHORIZED");
+
+        goTo(AUTHCODE_CLIENT_BASE_URL + "/info");
+        $("#logout").click();
+    }
+
 //    @Test
 //    public void abcUser() {
 //        goTo(AUTHCODE_CLIENT_BASE_URL + "/secured/abc");
