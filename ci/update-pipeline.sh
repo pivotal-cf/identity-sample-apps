ef79a05af20568656d58949f25437edd316a4858
Ryan Richard <rrichard@pivotal.io>
2020-01-13 13:03:57 -0800

    Switch pipeline to hush-house [#170635845]

--- ci/update-pipeline.sh
+++ ci/update-pipeline.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-fly -t wings set-pipeline \
+fly -t hush-house set-pipeline \
     -p identity-sample-apps-2.1 \
     -c "${script_dir}/pipeline.yml" \
     -l <(lpass show 'Sample Apps Concourse Secrets' --notes)
