39cdaf7f943e1679d742174490b2a68211334494
Ryan Richard <rrichard@pivotal.io>
2020-05-27 10:16:33 -0700

    Remove Spring Security from apps
    - and workarounds for known cf-for-k8s v0.1.0 limitations

--- authcode/src/main/java/io/pivotal/identityService/samples/authcode/Application.java
+++ authcode/src/main/java/io/pivotal/identityService/samples/authcode/Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+//		Thread.sleep(4000);
 		SpringApplication.run(Application.class, args);
 	}
 
--- resource-server/src/main/java/io/pivotal/identityService/samples/resourceserver/Application.java
+++ resource-server/src/main/java/io/pivotal/identityService/samples/resourceserver/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//        Thread.sleep(4000);
         SpringApplication.run(Application.class, args);
     }
 }
