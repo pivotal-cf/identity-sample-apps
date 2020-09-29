5126ccdf98713eb3e2cf659c5c36793a7f970192
Andrew Chang <anchang@vmware.com>
2020-09-29 14:28:11 -0700

    [#174887328] Add 2.3.1 journeys pipeline
    Signed-off-by: Margo Crawford <margaretc@vmware.com>

--- ci/journeys.sh
+++ ci/journeys.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 workspace_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
 
-fly -t wings execute \
+fly -t hush-house execute \
              --config $workspace_dir/identity-sample-apps/ci/tasks/journeys/task.yml \
              --input identity-sample-apps=$workspace_dir/identity-sample-apps \
              --input uaa=$workspace_dir/uaa \
--- ci/update-pipeline.sh
+++ ci/update-pipeline.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 fly -t hush-house set-pipeline \
-    -p identity-sample-apps-2.1 \
+    -p identity-sample-apps-2.3 \
     -c "${script_dir}/pipeline.yml" \
     -l <(lpass show 'Sample Apps Concourse Secrets' --notes)
