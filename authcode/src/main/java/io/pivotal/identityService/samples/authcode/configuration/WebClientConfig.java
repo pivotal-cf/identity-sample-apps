4d40097d685c5b78ae4fa989e3b384e562a9360a
Daniel Garnier-Moiroux <dgarnier@vmware.com>
2021-05-10 11:10:40 +0200

    Make samples work with self-signed certificates

--- authcode/src/main/java/io/pivotal/identityService/samples/authcode/configuration/WebClientConfig.java
+++ authcode/src/main/java/io/pivotal/identityService/samples/authcode/configuration/WebClientConfig.java
@@ -1,12 +1,24 @@
 package io.pivotal.identityService.samples.authcode.configuration;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLException;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -14,17 +26,40 @@ public class WebClientConfig {
     @Value("${RESOURCE_URL}")
     private String resourceServerUrl;
 
+    @Value("${TRUST_CERTS:}")
+    private String trustCerts;
+
+    private final Logger logger = LoggerFactory.getLogger(WebClientConfig.class);
+
     @Bean
     public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
-                               OAuth2AuthorizedClientRepository authorizedClientRepository) {
+                               OAuth2AuthorizedClientRepository authorizedClientRepository) throws Exception {
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
                 new ServletOAuth2AuthorizedClientExchangeFilterFunction(
                         clientRegistrationRepository,
                         authorizedClientRepository);
         oauth2.setDefaultOAuth2AuthorizedClient(true);
-        return WebClient.builder()
+        WebClient.Builder builder = WebClient.builder()
                 .baseUrl(resourceServerUrl)
-                .apply(oauth2.oauth2Configuration())
+                .apply(oauth2.oauth2Configuration());
+        if (shouldSkipSslValidation()) {
+            builder.clientConnector(insecureClientConnector());
+        }
+        return builder.build();
+    }
+
+    private boolean shouldSkipSslValidation() {
+        return !StringUtils.isEmpty(trustCerts);
+    }
+
+    private ReactorClientHttpConnector insecureClientConnector() throws SSLException, NoSuchAlgorithmException {
+        // #yolo trust everything
+        // This is an awful quickfix
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
                 .build();
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+        return new ReactorClientHttpConnector(httpClient);
     }
 }
